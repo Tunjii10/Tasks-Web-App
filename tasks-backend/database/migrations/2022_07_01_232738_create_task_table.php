821ad7c1f0da7e01bb6478b4df267436bdf5ad86
Tunjii10 <oluwatunji7@gmail.com>
2022-07-02 10:03:36 +0100

    database_migrations

---
+++ tasks-backend/database/migrations/2022_07_01_232738_create_task_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("title");
+            $table->string("description");
+            $table->string("comment")->nullable();
+            $table->enum("status", ['todo', 'done'])->default('todo');
+            $table->timestamp("planned_stop_date")->nullable();
+            $table->timestamp("actual_stop_date")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task');
+    }
+}
